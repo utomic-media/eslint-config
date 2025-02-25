8799b39c34b554acaa78976dbcf86e3610f074d5
Dominic <dominic.marcelino@utomic.media>
2025-02-25 19:44:15 +0100

    DOCS: update doc comment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import { config } from '@utomic-media/eslint-config';
 
 // Here we're importing our own configuration
 // This file is used for development and testing purposes
-// with the config-inspector tool
+// with the config-inspector tool (pnpm dev:inspect)
 
 export default [
   ...config.base,
